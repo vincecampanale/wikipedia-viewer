6be7426b62705a0f1966826ac22dfd633c713a7b
Vince Campanale <vcampanale0294@gmail.com>
2016-11-05 19:30:14 -0400

    angular.forEach is working

--- js/viewer.controller.js
+++ js/viewer.controller.js
@@ -1,13 +1,29 @@
 function ViewerController(ViewerService){
   var ctrl = this;
-  ctrl.results = [];
+  ctrl.fullResponse = [];
   ctrl.search = '';
+  ctrl.pages= '';
+  ctrl.pageIds = [];
+
+  ctrl.articles = [];
+
 
   ctrl.getPages = function(){
     ViewerService
       .retrieve(ctrl.search)
       .then(function(response){
-        ctrl.results = response;
+        ctrl.fullResponse = response.data;
+        ctrl.pages = ctrl.fullResponse.query.pages;
+        ctrl.pageIds = Object.keys(ctrl.pages);
+
+        angular.forEach(ctrl.pages, function(value, key){
+          ctrl.articles.push({
+            pageid: key,
+            title: value.title,
+            extract: value.extract
+          });
+        });
+
       });
   }
 }
--- js/viewer.service.js
+++ js/viewer.service.js
@@ -1,11 +1,11 @@
 function ViewerService($http){
   //TODO: add option for user to input how many results they would like (make gsrlimit into a variable)
 
-  var API = 'https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-  var callback = '&callback=JSON_CALLBACK';
+  var API = 'https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=5&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+  //var callback = '&callback=JSON_CALLBACK';
 
   function retrieve(search){
-    return $http.get(API + search + callback).then(function(response){
+    return $http.get(API + search).then(function(response){
       return response;
     });
   }
